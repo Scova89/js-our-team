cf0966d09c53820a8e01dfd4301466be7491518d
Francesco <scovascova@gmail.com>
2021-11-19 16:34:42 +0100

    creato array del team

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const arrayTeam = [
+    {
+        "imm":"img/wayne-barnett-founder-ceo.jpg",
+        "nome": "Wayne Barnett",
+        "ruolo": "Founder & CEO"
+    },
+
+    {
+        "imm":"img/angela-caroll-chief-editor.jpg",
+        "nome": "Angela Caroll",
+        "ruolo": "Chief Editor"
+    },
+
+    {
+        "imm":"img/walter-gordon-office-manager.jpg",
+        "nome": "Walter Gordon",
+        "ruolo": "Office Manager"
+    },
+
+    {
+        "imm":"img/angela-lopez-social-media-manager.jpg",
+        "nome": "Angela Lopez",
+        "ruolo": "Social Media Manager"
+    },
+
+    {
+        "imm":"img/scott-estrada-developer.jpg",
+        "nome": "Scott Estrada",
+        "ruolo": "Developer"
+    },
+
+    {
+        "imm":"img/barbara-ramos-graphic-designer.jpg",
+        "nome": "Barbara Ramos",
+        "ruolo": "Graphic Designer"
+    },
+];
+
+console.log(arrayTeam);
+
+
